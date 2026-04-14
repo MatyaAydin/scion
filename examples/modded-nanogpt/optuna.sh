847dae4f379844e0557ae84c961c219eef524bcf
MatyaAydin <119845483+MatyaAydin@users.noreply.github.com>
2026-04-14 20:56:19 +0200

    ad missing import

--- examples/modded-nanogpt/optuna.sh
+++ examples/modded-nanogpt/optuna.sh
@@ -3,5 +3,6 @@
 pip install -r requirements.txt
 pip install -r data/requirements.txt
 pip install optuna
+pip install optuna-integration[pytorch_distributed]
 
 torchrun --standalone --nproc_per_node=4 train_gpt_optuna.py
