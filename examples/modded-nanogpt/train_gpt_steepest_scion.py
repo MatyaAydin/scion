71cd34c538f332f7890fc510daea82d7a638354c
MatyaAydin <119845483+MatyaAydin@users.noreply.github.com>
2026-04-16 16:10:55 +0200

    who the fuck removed warmup iters ???

--- examples/modded-nanogpt/train_gpt_steepest_scion.py
+++ examples/modded-nanogpt/train_gpt_steepest_scion.py
@@ -248,7 +248,7 @@ class Hyperparameters:
     sequence_length : int = 1024 # sequence length, in tokens
     num_iterations : int = 5100 # number of iterations to run
     learning_rate : float = 1e-5#0.00036
-    warmup_iters : int = 0
+    warmup_iters : int = 500
     warmdown_iters : int = 1450 # number of iterations of linear warmup/warmdown for triangular or trapezoidal schedule
     weight_decay : float = 0
     # evaluation and logging hyperparams
