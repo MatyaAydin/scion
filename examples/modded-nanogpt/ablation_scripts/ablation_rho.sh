e1b7b9be2b105a058bf4f33666550348381f9e41
MatyaAydin <119845483+MatyaAydin@users.noreply.github.com>
2026-05-21 15:01:36 +0200

    fix: unconstr flag

--- examples/modded-nanogpt/ablation_scripts/ablation_rho.sh
+++ examples/modded-nanogpt/ablation_scripts/ablation_rho.sh
@@ -23,6 +23,6 @@ cd /iopsstor/scratch/cscs/maydin/scion/examples/modded-nanogpt
 pip install -r ./requirements.txt
 pip install -r ./data/requirements.txt
 
-torchrun --standalone --nproc_per_node=4 ./train_mousse_scion.py --thresh ${R} --log-dir ${LOGDIR} --unconstrained True
+torchrun --standalone --nproc_per_node=4 ./train_mousse_scion.py --thresh ${R} --log-dir ${LOGDIR} --unconstrained
 EOF
 done
