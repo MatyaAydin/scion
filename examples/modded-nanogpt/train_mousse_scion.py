04c48f7ecdf974596f1d22663d8d3979eb20553b
MatyaAydin <119845483+MatyaAydin@users.noreply.github.com>
2026-05-25 15:57:47 +0200

    forgot to change grafitn

--- examples/modded-nanogpt/train_mousse_scion.py
+++ examples/modded-nanogpt/train_mousse_scion.py
@@ -512,7 +512,7 @@ if __name__ == "__main__":
         "eig_update_freq": 125,
         "eps":1e-8,
         "alpha": args.alpha,
-        "apply_grafting": "dual",
+        "apply_grafting": "interpolate",
         "norm_warmup_steps": args.num_iterations / 10.,
         "eig_schedule": None#eig_schedule
     }
