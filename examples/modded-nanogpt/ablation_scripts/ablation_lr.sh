1ef7c4718d0563109cdf408a8fbda025efa3fb67
MatyaAydin <119845483+MatyaAydin@users.noreply.github.com>
2026-05-25 13:54:04 +0200

    lr sweep

--- examples/modded-nanogpt/ablation_scripts/ablation_lr.sh
+++ examples/modded-nanogpt/ablation_scripts/ablation_lr.sh
@@ -2,8 +2,8 @@
 
 cd ..
 
-LEARNING_RATES=(5e-3 3e-3 2e-3 1.5e-3 1.2e-3)
-LOGDIR="logs_lr"
+LEARNING_RATES=(1e-3 5e-3 7.5e-3 1e-2 4e-2)
+LOGDIR="logs_lr_dual"
 
 for LR in "${LEARNING_RATES[@]}"; do
     sbatch <<EOF
@@ -24,6 +24,6 @@ cd /iopsstor/scratch/cscs/maydin/scion/examples/modded-nanogpt
 pip install -r ./requirements.txt
 pip install -r ./data/requirements.txt
 
-torchrun --standalone --nproc_per_node=4 ./train_mousse_scion.py --lr ${LR} --log_dir ${LOGDIR}
+torchrun --standalone --nproc_per_node=4 ./train_mousse_scion.py --lr ${LR} --eig-update-freq 125 --log-dir ${LOGDIR}
 EOF
 done
