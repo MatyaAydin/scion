8d65d8ca3b4fca6ceaf8f2883916182b91791981
MatyaAydin <119845483+MatyaAydin@users.noreply.github.com>
2026-06-03 19:51:46 +0200

    abl ns

--- examples/modded-nanogpt/ablation_scripts/ablation_ns.sh
+++ examples/modded-nanogpt/ablation_scripts/ablation_ns.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 STEPS=(2 3 4 5 6)
-LOGDIR="logs_ns"
+LOGDIR="logs_prescion_ns"
 
 for S in "${STEPS[@]}"; do
     sbatch <<EOF
--- examples/modded-nanogpt/train_mousse_scion.py
+++ examples/modded-nanogpt/train_mousse_scion.py
@@ -364,6 +364,7 @@ def main(args, optim_args):
     # begin logging
     if master_process:
         study_name = "logs_" + "".join([f"{k}_{optim_args[k]}_" for k in optim_args.keys() if k != "eig_schedule"])
+        study_name += f"ns_steps_{args.ns_steps}"
         if optim_args.get("eig_schedule", ""):
             study_name += "".join([f"{k}_{optim_args["eig_schedule"][k]}_" for k in optim_args["eig_schedule"].keys()])
         # logdir = f'logs/{study_name}'
