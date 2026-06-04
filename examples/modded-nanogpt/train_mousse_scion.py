4dc7c2299f4703d6a02014f8f4b8c9fd0a04fe26
MatyaAydin <119845483+MatyaAydin@users.noreply.github.com>
2026-06-04 11:45:05 +0200

    missing

--- examples/modded-nanogpt/train_mousse_scion.py
+++ examples/modded-nanogpt/train_mousse_scion.py
@@ -522,7 +522,7 @@ if __name__ == "__main__":
         "norm_warmup_steps": args.num_iterations / 10.,
         "beta_scale": args.beta_scale,
         "eig_schedule": None,#eig_schedule,
-        "use_trace_normalization": False,#args.use_trace_normalization,
+        "use_trace_normalization": True,#args.use_trace_normalization,
         "LR_correction": False#args.LR_correction
     }
 
